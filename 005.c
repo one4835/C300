a8ca48031f2e227b36eb5994a5b615518233bbaa
one4835 <57549928+one4835@users.noreply.github.com>
2019-11-17 20:28:14 +0900

    Create 005.c

---
+++ 005.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+    int x;
+    int y;
+
+    x = 20;
+
+    y = 10;
+
+    if(x > y)
+    {
+        printf("x의 값이 y보다 큽니다.");
+    }
+    else
+    {
+        printf("x의 값이 y보다 작거나 같습니다.");
+    }
+}
