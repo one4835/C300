a6417099a17a9411d94bf3e6e2d5e1b5ba7bce0e
one4835 <57549928+one4835@users.noreply.github.com>
2020-01-08 10:04:38 +0900

    1

---
+++ 101~110/109.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_pxy(int*pxy[2]);
+
+void main(void)
+{
+    int x = 0,y = 0;
+    int*pxy[2];
+
+    pxy[0] = &x;
+    pxy[1] = &y;
+
+    *pxy[0] = 5;
+    *pxy[1] = 10;
+
+    print_pxy(pxy);
+}
+
+void print_pxy(int*pxy[2])
+{
+
+    printf("pxy[0]= %d\n",x,*pxy[0]);
+    printf("pxy[1]= %d\n",y,*pxy[1]);
+}
