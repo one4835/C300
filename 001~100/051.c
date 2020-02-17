8b478d9a3daf9f2dcf4f37dca0495e876f6f64ac
one4835 <57549928+one4835@users.noreply.github.com>
2020-02-17 16:51:23 +0900

    2020.2.17.2

---
+++ 001~100/051.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define ENTER 13
+
+void main(void)
+{
+    int ch;
+
+    printf("Press the key that you want to change to ASCII cord...\n");
+    printf("If you press Enter, program would end\n");
+
+    do
+    {
+        ch = getch();
+
+        printf("text :(%c), ASCII cord = (%d)\n",ch,ch);
+        
+    }while(ch != ENTER);
+}
